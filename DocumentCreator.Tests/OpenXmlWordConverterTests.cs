8f9291e92447741c9dea2fe16402e087779dfe95
Panos <pkokki@gmail.com>
2020-05-09 10:45:11 +0300

    WIP: html convert with AlternateContent

--- DocumentCreator.Tests/OpenXmlWordConverterTests.cs
+++ DocumentCreator.Tests/OpenXmlWordConverterTests.cs
@@ -15,5 +15,15 @@ namespace DocumentCreator
             Assert.NotNull(conversion.Html);
             Assert.Equal(2, conversion.Images.Count);
         }
+
+        [Fact]
+        public void CanConvertWhenAlternateContent()
+        {
+            using var ms = new MemoryStream();
+            ms.Write(Resources.simple_receipt_template_docx, 0, Resources.simple_receipt_template_docx.Length);
+            var conversion = OpenXmlWordConverter.ConvertToHtml(ms, "DOC1");
+            Assert.NotNull(conversion);
+            Assert.NotNull(conversion.Html);
+        }
     }
 }
